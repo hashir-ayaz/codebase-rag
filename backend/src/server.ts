8970a9b367aac0b56dc4d02bab02413a61665b96
hashir ayaz <hashirsmartyy@gmail.com>
2025-01-06 11:39:42 +0000

    Added vps to cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -18,7 +18,7 @@ import cors from "cors";
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http://143.110.183.231:5173"],
     credentials: true,
   })
 );
